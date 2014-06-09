1980ef258f02966be4869eacbb535f233cbf77f9
sorpa'as plat <sorpaas@gmail.com>
2014-06-09 11:51:56 +0800

    finish json provider

--- src/providers/json_provider.js
+++ src/providers/json_provider.js
@@ -1,15 +1,39 @@
-module.exports = function(physical_path) {
-  return {
-    read: function(path) {
+var fs = require('fs');
+var path = require('../path');
 
-    },
+module.exports = function(physicalPath) {
+  var rawData = JSON.parse(fs.readFileSync(physicalPath, {encoding: 'utf8'}));
 
-    write: function(path) {
-
-    },
-
-    execute: function(path) {
-
+  function read(path) {
+    var routes = path.normalize(path).split('/');
+    var currentObj = rawData;
+    for(var i in routes) {
+      currentObj = currentObj[routes[i]];
     }
+    return currentObj;
+  }
+
+  function write(path, data) {
+    var routes = path.normalize(path).split('/');
+    var currentObj = rawData;
+    for(var i in routes) {
+      if(routes[i] == routes.length - 1) {
+        currentObj[routes[i]] = data;
+      } else {
+        currentObj = currentObj[routes[i]];
+      }
+    }
+    fs.writeFileSync(path, rawData, {});
+    return true;
+  }
+
+  function execute(path) {
+    return read(path)();
+  }
+
+  return {
+    read: read,
+    write: write,
+    execute: execute
   }
 }
